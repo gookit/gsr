05fdce313fa2fd3d74c7f362dab7ff685098a682
inhere <in.798@qq.com>
2021-06-27 13:19:52 +0800

    cache: add new method GetAs()

--- simple_cache.go
+++ simple_cache.go
@@ -14,12 +14,14 @@ type SimpleCacher interface {
 
 	// Has basic operation
 	Has(key string) bool
-	Get(key string) interface{}
-	Set(key string, val interface{}, ttl time.Duration) (err error)
 	Del(key string) error
+	Get(key string) interface{}
+	// GetAs get cache value and unmarshal as ptr.
+	GetAs(key string, ptr interface{}) error
+	Set(key string, val interface{}, ttl time.Duration) error
 
 	// GetMulti multi operation
 	GetMulti(keys []string) map[string]interface{}
-	SetMulti(values map[string]interface{}, ttl time.Duration) (err error)
+	SetMulti(values map[string]interface{}, ttl time.Duration) error
 	DelMulti(keys []string) error
 }
