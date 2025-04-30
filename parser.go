ea8560852d94fc119ae473d5d5b869ba4617d1ea
Inhere <inhere@users.noreply.github.com>
2025-04-30 10:38:33 +0800

    Update parser.go - add some new interface

--- parser.go
+++ parser.go
@@ -1,5 +1,32 @@
 package gsr
 
+
+// Serializer interface definition
+type Serializer interface {
+	Serialize(v any) ([]byte, error)
+	Deserialize(data []byte, v any) error
+}
+
+// GoSerializer interface definition
+type GoSerializer interface {
+	Marshal(v any) ([]byte, error)
+	Unmarshal(v []byte, ptr any) error
+}
+
+// DataParser interface
+// Deperacted
+type DataParser = GoSerializer
+
+// Codec interface definition
+type Codec interface {
+	Decode(blob []byte, v any) (err error)
+	Encode(v any) (out []byte, err error)
+}
+
+//
+//
+//
+
 // Marshaler interface
 type Marshaler interface {
 	Marshal(v any) ([]byte, error)
@@ -8,12 +35,6 @@ type Marshaler interface {
 // Unmarshaler interface
 type Unmarshaler interface {
 	Unmarshal(v []byte, ptr any) error
-}
-
-// DataParser interface for Marshal/Unmarshal data
-type DataParser interface {
-	Marshaler
-	Unmarshaler
 }
 
 // MarshalFunc define
@@ -31,3 +52,10 @@ type UnmarshalFunc func(v []byte, ptr any) error
 func (u UnmarshalFunc) Unmarshal(v []byte, ptr any) error {
 	return u(v, ptr)
 }
+
+// DecodeFunc definition
+type DecodeFunc func(blob []byte, v any) (err error)
+
+// EncodeFunc definition
+type EncodeFunc func(v any) (out []byte, err error)
+
